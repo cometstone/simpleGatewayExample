bcd8142d0293a1be7e1dc60520b5c16f5e6e9b63
liao <80153517@qq.com>
2017-11-09 10:39:15 +0800

    manager 管理接口

---
+++ manager/manager.go
@@ -0,0 +1,32 @@
+package manager
+
+import (
+	"go.uber.org/zap"
+	"simpleGatewayExample/common"
+	"database/sql"
+	"simpleGatewayExample/sdk"
+	"simpleGatewayExample/global/servicelist"
+)
+
+var Looger *zap.Logger
+var Conf *common.Config
+
+var db *sql.DB
+
+func Start() {
+	common.InitConfig()
+	Conf = common.Conf
+
+	sdk.InitLogger(Conf.Common.LogLevel,Conf.Common.LogPath,Conf.Common.IsDebug,servicelist.SimpleGatewayExampleManager)
+	db = sdk.InitMysql(&sdk.MysqlConf{
+		Acc:		Conf.Mysql.Acc,
+		Pw:			Conf.Mysql.Pw,
+		Addr:		Conf.Mysql.Addr,
+		Port:		Conf.Mysql.Port,
+		Database:	Conf.Mysql.Database,
+	})
+
+	//初始化 获取在活的服务器列表
+	initGatewayUpdate()
+
+}
